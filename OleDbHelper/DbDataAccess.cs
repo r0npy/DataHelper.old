e50df621047fc2ef0f386f7f3b2bc163525cb124
Ronald Riveros <ronaldriveros@gmail.com>
2020-08-19 20:32:42 -0400

    prueba nomas

--- OleDbHelper/DbDataAccess.cs
+++ OleDbHelper/DbDataAccess.cs
@@ -10,7 +10,7 @@ using System.Threading.Tasks;
 /// </summary>
 abstract class DbDataAccess
 {
-    #region Declaración de Variables
+    #region Declaración de Variables d
 
     protected string Servidor { get; set; }
     protected string BaseDatos { get; set; }
